03fba239c528c0108d80031c27bdb157225125e9
Rohit Kumar Malik <rkmalik@ufl.edu>
2015-09-12 09:13:14 -0400

    Daily practice

---
+++ programming/competition/algorithms/src/leetcode/prob241/Main.java
@@ -0,0 +1,17 @@
+package leetcode.prob241;
+
+import java.util.HashMap;
+
+public class Main {
+	
+	
+	public static void main (String args[]) {
+		
+	    Solution sol = new Solution ();
+	    boolean istrue = sol.isAnagram("anagram", "mogaram");
+	    
+	    System.out.println(istrue);
+		
+	}
+}
+
