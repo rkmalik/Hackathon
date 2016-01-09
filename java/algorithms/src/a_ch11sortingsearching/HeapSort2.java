5808788e45929442e7be27346b0755b7b3d279f5
rkmalik <rkmalik@ufl.edu>
2016-01-09 01:36:29 -0500

    Code till Jan 8 2016

--- java/algorithms/src/a_ch11sortingsearching/HeapSort2.java
+++ java/algorithms/src/a_ch11sortingsearching/HeapSort2.java
@@ -31,68 +31,6 @@ public class HeapSort2 {
 		
 	}
 
-	public static void maxHeapify(int arr[], int i, int size) {
-
-		int left = leftChild(i);
-		int right = rightChild(i);
-
-		int max = i;
-
-		if (left <= size && arr[max] < arr[left]) {
-			max = left;
-		}
-
-		if (right <= size && arr[max] < arr[right]) {
-			max = right;
-		}
-
-		if (max != i) {
-			int temp = arr[i];
-			arr[i] = arr[max];
-			arr[max] = temp;
-			maxHeapify(arr, max, size);
-		}
-
-		
-	}
-
-	public static void buildMaxheap(int arr[]) {
-
-		int lastindex = arr.length - 1;
-
-		for (int i = lastindex / 2; i >= 0; i--) {
-			maxHeapify(arr, i, arr.length - 1);
-		}
-	}
-
-	public static void heapSort(int arr[]) {
-
-		buildMaxheap(arr);
-
-		int i = 0;
-		int size = arr.length - 1;
-
-		while (size > 0) {
-
-			int temp = arr[0];
-			arr[0] = arr[size];
-			arr[size] = temp;
-			size--;
-			maxHeapify(arr, 0, size);
-		}
-
-	}
-
-	private static int parent(int i) {
-		return ((i - 1) / 2);
-	}
-
-	private static int leftChild(int i) {
-		return (2 * i + 1);
-	}
-
-	private static int rightChild(int i) {
-		return (2 * i + 2);
-	}
+	
 
 }
